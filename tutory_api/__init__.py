8066ee122ccb843dbd9861637ac1a0d0c97fd533
CK <chuunkiat@gmail.com>
2020-08-26 19:10:32 +0800

    dashboard announcement & activity

--- tutory_api/__init__.py
+++ tutory_api/__init__.py
@@ -7,7 +7,6 @@ from tutory_api.blueprints.users.views import users_api_blueprint
 from tutory_api.blueprints.sessions.views import sessions_api_blueprint
 from tutory_api.blueprints.dashboard.views import dashboard_api_blueprint
 
-
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
 app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1/sessions')
 app.register_blueprint(dashboard_api_blueprint, url_prefix='/api/v1/dashboard')
--- tutory_api/blueprints/dashboard/views.py
+++ tutory_api/blueprints/dashboard/views.py
@@ -3,15 +3,16 @@ from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.user import User
 from models.announcement import Announcement
+from models.activities import Activities
 
 dashboard_api_blueprint = Blueprint('dashboard_api',
                              __name__,
                              template_folder='templates')
 
 
-@dashboard_api_blueprint.route('/<id>', methods=['GET'])
+@dashboard_api_blueprint.route('/announcement/', methods=['GET'])
 @jwt_required
-def home():
+def home_announcement():
     username = get_jwt_identity()
     user = User.get_or_none(User.identity_card == username)
 
@@ -19,60 +20,196 @@ def home():
         result = []
         for announcement in Announcement.select():
             result.append({
-                "post_id": announcement.id,
-                "name": announcement.full.name,
-                "title": announcement.title,
-                "post": announcement.post
+                    "post_id": announcement.id,
+                    "staff_name": announcement.full_name,
+                    "title": announcement.title,
+                    "post": announcement.post
+            })
+        return jsonify(result)
+
+@dashboard_api_blueprint.route('/activities/', methods=['GET'])
+@jwt_required
+def home_activities():
+    username = get_jwt_identity()
+    user = User.get_or_none(User.identity_card == username)
+
+    if user:
+        result = []
+        for activity in Activities.select():
+            result.append({
+                    "post_id": activity.id,
+                    "staff_name": activity.full_name,
+                    "title": activity.title,
+                    "post": activity.post
             })
         return jsonify(result)
 
 
-@dashboard_api_blueprint.route('/update', methods=['POST'])
+@dashboard_api_blueprint.route('/announcement/update', methods=['POST'])
 @jwt_required
 def new_announcement():
-    # staff_name = request.json.get("full_name")
-    # new_title = request.json.get("title")
-    # new_post = request.json.get("post")
-
-    user = get_jwt_identity
-    params = request.json
-    new_announcements = Announcement(full_name=params.get("full_name"), title=params.get("title"),post=params.get("post"))
-
-    if User.get_or_none(User.full_name == staff_name) and User.roles == "staff":
-        if new_announcements.save():
-            pass
-
-
-
-
-@dashboard_api_blueprint.route('/edit', methods=['POST'])
-@jwt_required
-def edit_existing_post():
     user = get_jwt_identity()
-    if user.roles == 'staff':
-        post_id = request.json.get("post_id")
-        updated_title = request.json.get("title")
-        updated_post = request.json.get("post")
-        post_to_edit = Announcement.get_by_id(Announcement.id == post_id)
-        post_to_edit.title = updated_title
-        post_to_edit.post = updated_post
+    params = request.json
+    staff = User.get_or_none(User.identity_card == get_jwt_identity())
+    new_announcements = Announcement(full_name=params.get("full_name"), identity_card=params.get("identity_card"), title=params.get("title"),post=params.get("post"))
 
-        if post_to_edit.save():
-            result = []
+    if staff.roles == "staff":
+        if new_announcements.save():
+            response = []
             for announcement in Announcement.select():
-                result.append({
+                response.append({
+                    "message": "New announcement successfully updated",
+                    "status": "Success",
                     "post_id": announcement.id,
-                    "name": announcement.full.name,
+                    "staff_name": announcement.full_name,
                     "title": announcement.title,
                     "post": announcement.post
                 })
-            return jsonify(result)
         else:
-            return jsonify({"error":"Edit failed, please try again"})
+            response = {
+                "message": "Update failed, please try again",
+                "status": "Failed"
+            }
+    else:
+        response = {"message": "You are not allowed to perform this action!"}
+    return jsonify(response)
 
 
+@dashboard_api_blueprint.route('/activities/update', methods=['POST'])
+@jwt_required
+def new_activity():
+    user = get_jwt_identity()
+    params = request.json
+    staff = User.get_or_none(User.identity_card == get_jwt_identity())
+    new_activity = Activities(full_name=params.get("full_name"), identity_card=params.get("identity_card"), title=params.get("title"),post=params.get("post"))
 
-@dashboard_api_blueprint.route('/delete', methods=['DELETE'])
+    if staff.roles == "staff":
+        if new_activity.save():
+            response = []
+            for activity in Activities.select():
+                response.append({
+                    "message": "New activity successfully updated",
+                    "status": "Success",
+                    "post_id": activity.id,
+                    "staff_name": activity.full_name,
+                    "title": activity.title,
+                    "post": activity.post
+                })
+        else:
+            response = {
+                "message": "Update failed, please try again",
+                "status": "Failed"
+            }
+    else:
+        response = {"message": "You are not allowed to perform this action!"}
+    return jsonify(response)
+
+
+@dashboard_api_blueprint.route('/announcement/edit', methods=['POST'])
+@jwt_required
+def edit_announcement_post():
+    user = get_jwt_identity()
+    params = request.json
+    staff = User.get_or_none(User.identity_card == get_jwt_identity())
+    edit_post_announcement = Announcement(id=params.get("id"), title=params.get("title"),post=params.get("post"))
+
+    if staff.roles == "staff":
+        if edit_post_announcement.save():
+            for announcement in Announcement.select():
+                response = {
+                    "message": "Announcement successfully edited",
+                    "status": "Success",
+                    "post_id": announcement.id,
+                    "staff_name": announcement.full_name,
+                    "title": announcement.title,
+                    "post": announcement.post
+                    }
+        else:
+            response = {
+                "message": "Edit failed, please try again",
+                "status": "Failed"
+            }
+    else:
+        response = {"message": "You are not allowed to perform this action!"}
+    return jsonify(response)
+
+
+@dashboard_api_blueprint.route('/activities/edit', methods=['POST'])
+@jwt_required
+def edit_activity_post():
+    user = get_jwt_identity()
+    params = request.json
+    staff = User.get_or_none(User.identity_card == get_jwt_identity())
+    edit_post_activity = Activities(id=params.get("id"), title=params.get("title"),post=params.get("post"))
+
+    if staff.roles == "staff":
+        if edit_post_activity.save():
+            for activity in Activities.select():
+                response = {
+                    "message": "Activity successfully edited",
+                    "status": "Success",
+                    "post_id": activity.id,
+                    "staff_name": activity.full_name,
+                    "title": activity.title,
+                    "post": activity.post
+                    }
+        else:
+            response = {
+                "message": "Edit failed, please try again",
+                "status": "Failed"
+            }
+    else:
+        response = {"message": "You are not allowed to perform this action!"}
+    return jsonify(response)
+
+
+@dashboard_api_blueprint.route('/announcement/delete', methods=['DELETE'])
 @jwt_required
 def delete_announcement():
-    pass
+    user = get_jwt_identity()
+    params = request.json
+    staff = User.get_or_none(User.identity_card == get_jwt_identity())
+    delete_post_announcement = Announcement.get_by_id(params.get("id"))
+
+    if staff.roles == "staff":
+        if delete_post_announcement.delete_instance():
+            for announcement in Announcement.select():
+                response = {
+                    "message": "Announcement successfully deleted",
+                    "status": "Success",
+                    "post_id_deleted": delete_post_announcement.id
+                }
+        else:
+            response = {
+                "message": "Deleted failed, please try again",
+                "status": "Failed"
+            }
+    else:
+        response = {"message": "You are not allowed to perform this action!"}
+    return jsonify(response)
+
+
+@dashboard_api_blueprint.route('/activity/delete', methods=['DELETE'])
+@jwt_required
+def delete_activity():
+    user = get_jwt_identity()
+    params = request.json
+    staff = User.get_or_none(User.identity_card == get_jwt_identity())
+    delete_post_activity = Activities.get_by_id(params.get("id"))
+
+    if staff.roles == "staff":
+        if delete_post_activity.delete_instance():
+            for activity in Activities.select():
+                response = {
+                    "message": "Activity successfully deleted",
+                    "status": "Success",
+                    "post_id_deleted": delete_post_activity.id
+                }
+        else:
+            response = {
+                "message": "Deleted failed, please try again",
+                "status": "Failed"
+            }
+    else:
+        response = {"message": "You are not allowed to perform this action!"}
+    return jsonify(response)
